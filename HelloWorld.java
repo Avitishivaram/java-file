c7b1bc83bacf33b718ec27116673830929a95793
Avitishivaram <shivaramavuiti2000@gmail.com>
2024-05-24 12:50:41 +0530

    changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld
 { 
 	public static void main (String args[])
 	{
-		System.out.print("Hello hii");
-		System.out.print("ruthvikkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+		System.out.print("Hello hai");
+		System.out.print("hpw are you friends");
 	}
 }
-}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
