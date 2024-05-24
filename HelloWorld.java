5b1f1df2acdaee3125124bf5f9b1ee5129776f39
Hima4545 <reddyvarsha05@gmail.com>
2024-05-24 12:52:48 +0530

    worst

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 	public static void main (String args[])
 	{
 		System.out.print("Hello hai");
-		System.out.print("hpw are you friends");
+		System.out.print("hpw are you friends....................");
 	}
 }
